2bcd56a1231a715d98e33e9dec065c832db70197
Anton Fagerberg <anton@antonfagerberg.com>
2014-05-14 21:49:02 +0200

    First commit!

---
+++ Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	private Vector3 movement;
+
+	public int movementSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movement = transform.position;
+
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			movement.y += movementSpeed * Time.deltaTime;
+		} else if (Input.GetKey(KeyCode.DownArrow)) {
+			movement.y -= movementSpeed * Time.deltaTime;
+		}
+		
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			movement.x -= movementSpeed * Time.deltaTime;
+		} else if (Input.GetKey(KeyCode.RightArrow)) {
+			movement.x += movementSpeed * Time.deltaTime;
+		}
+
+		transform.position = movement;
+	}
+}
